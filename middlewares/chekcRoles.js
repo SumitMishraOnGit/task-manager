862dafafcc5fec51a82ff8082455d0a262246ef9
Sumit Mishra <sumitmishra9606@gmail.com>
2025-05-03 22:37:27 +0530

    improved RBAC by implementing editors and viewer fields

--- middlewares/chekcRoles.js
+++ middlewares/chekcRoles.js
@@ -1,13 +1,16 @@
 const checkRole = (allowedRoles) => {
-    return (req, res, next) => {
-      const userRole = req.user.role;
-      if (!allowedRoles.includes(userRole)) {
-        return res.status(403).json({ message: "Access denied: You dont have access to be HERE!" });
-      }
-      next();
-    };
-  };
+  return (req, res, next) => {
+    const userRoles = req.user.roles || [];
+    const isVirtualAdmin = req.user.roles.includes("editor") && req.user.roles.includes("viewer");
 
-  
-module.exports = checkRole;
-  
+    const hasRole = allowedRoles.some(role => 
+      userRoles.includes(role) || (role === "admin" && isVirtualAdmin)
+    );
+
+    if (!hasRole) {
+      return res.status(403).json({ message: "Access denied." });
+    }
+
+    next();
+  };
+};
